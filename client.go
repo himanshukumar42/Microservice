65cf7c8738cf314d6aa9028d5c3088c3ec42eafb
Abhishek Singh <abhishek.singh@cloudanalogy.com>
2022-05-06 14:27:41 +0530

    Initial Commit

---
+++ client.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/himanshuk42/microservice/chat"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := chat.NewChatServiceClient(conn)
+
+	message := chat.Message{
+		Body: "Hello from the Client",
+	}
+	response, err := c.SayHello(context.Background(), &message)
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+
+	log.Printf("Response from Server: %s", response.Body)
+}
